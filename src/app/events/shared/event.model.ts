cd44740744de0e8c64852e9741f0effb1792e913
Buhari Alhassan Ahmed <50429337+makajeez@users.noreply.github.com>
2019-12-12 18:01:04 +0100

    events interface

---
+++ src/app/events/shared/event.model.ts
@@ -0,0 +1,24 @@
+export interface IEvent {
+    id: number;
+    name: string;
+    date: string;
+    time: string;
+    price: number;
+    imageUrl: string;
+    location?: {
+        address: string
+        city: string
+        country: string
+    };
+    onlineUrl?: string;
+    sessions: ISession[];
+}
+export interface ISession {
+    id: number;
+    name: string;
+    presenter: string;
+    duration: string;
+    level: string;
+    abstract: string;
+    voters: string[];
+}
