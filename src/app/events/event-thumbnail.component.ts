c5bf194eb617f1ce964b02113aa714008bc6a737
Buhari Alhassan Ahmed <50429337+makajeez@users.noreply.github.com>
2019-12-13 11:00:03 +0100

    event thumbnail component model

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input} from '@angular/core'
-
+import { Component, Input} from '@angular/core'; // Input was also imported to allow communication
+import { IEvent } from './shared/event.model';
 @Component({
     selector: 'event-thumbnail',
-    template:`
+    template: `
     <div>
-        <div class="well hoverwell thumbnail">
-            <h2>{{event?.name}}</h2>
+        <div [routerLink] = "['/events',event.id]" class="well hoverwell thumbnail">
+            <h2>{{event?.name | uppercase}}</h2> 
             <div>Date: {{event?.date}}</div>
-            <div>Time: {{event?.time}}</div>
-            <div>Price: \${{event?.price}}</div>
-            <div>
+            <div [ngClass]="getStartTimeClass()" [ngSwitch] = "event?.time">Time: {{event?.time}}
+                <span *ngSwitchCase = "'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase = "'9:00 am'">(Normal Start)</span>
+                <span *ngSwitchDefault>(Late Start)</span>
+            </div>
+            <div>Price: \${{event?.price}}</div><!--we can use pipe to format the currency-->
+            <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
                 <span class="pad-left">{{event?.location?.city}},{{event.location?.country}}</span>
             </div>  
