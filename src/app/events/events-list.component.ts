f2c3770facc35a5c8ae53b3c42a3253f160d7611
Buhari Alhassan Ahmed <50429337+makajeez@users.noreply.github.com>
2019-12-13 11:32:12 +0100

    events cmponent

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
+import { ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared/event.model';
 
+declare let toastr;
 @Component({
     selector:'events-list',
     template: `
