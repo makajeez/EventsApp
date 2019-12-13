08e6d9734d92ebf13266edbdf51d3fb920fa0a32
Buhari Alhassan Ahmed <50429337+makajeez@users.noreply.github.com>
2019-12-13 15:06:33 +0100

    module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { EventsAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
-import { NavBarComponent } from './nav/navbar.component'
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router';
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver,
+    CreateSessionComponent,
+    SessionListComponent
+} from './events/index';
+import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/toastr.service';
+import { CollapsibleWellComponent } from './common/collapsible-well.component'
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventsListComponent, 
+    EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    EventDetailsComponent,
+    NavBarComponent,
+    CreateEventComponent,
+    Error404Component,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    AuthService,
+    EventListResolver,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkState
+    }
+   ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('you have not saved this event,do you really want to cancel?');
+  }
+  return true;
+}
